7fa44b2a3c9b55c3f779d943bce4ba5514745861
Matt Minich <mminich@wisc.edu>
2020-08-07 12:51:26 -0500

    Update Scraper.py
    Added code for scraping the comment forest for each post.

--- Scraper.py
+++ Scraper.py
@@ -14,26 +14,41 @@ subreddit = subterm.hot(limit=limit)
 #
 
 with open('%s.csv' %term,'w', encoding = 'utf-8') as csvfile:
-    fieldnames = ['title','author','original content','selfpost','time created','stickied','locked','NSFW','selftext','number of comments','score','upvote ratio','permalink','url']
+    fieldnames = ['title','author','original content','selfpost','stickied','selftext','comment forest','time created','locked','number of comments','NSFW','permalink','score','upvote ratio','url']
     filewriter = csv.DictWriter(csvfile, fieldnames = fieldnames)
     filewriter.writeheader()
-
     for i in subreddit:
-
+        submission = reddit.submission(id=i.id)
+        submission.comments.replace_more(limit = None)
         ts = datetime.utcfromtimestamp(i.created_utc).strftime('%Y-%m-%d %H:%M:%S')
-        filewriter.writerow({'title':i.title,
-                            'author':i.author,
-                            'original content':i.is_original_content,
-                            'selfpost':i.is_self,
-                            'time created':ts,
-                            'stickied':i.stickied,
-                            'locked':i.locked,
-                            'NSFW':i.over_18,
-                            'selftext':i.selftext,
-                            'number of comments':i.num_comments,
-                            'score':i.score,
-                            'upvote ratio':i.upvote_ratio,
-                            'permalink':i.permalink,                
-                            'url':i.url})
+        tree = "Comment section: \n"
+        for comment in submission.comments.list():
+            author = str(comment.author)
+            body = str(comment.body)
+            score = str(comment.score)
+            cmnt = ('### \n COMMENT: ' + body + ' [author (' + author + ') score (' + score + ')]')
+            tree = (tree + cmnt)
+            if len(comment.replies) > 0:
+               for reply in comment.replies:
+                  author = str(reply.author)
+                  body = str(reply.body)
+                  score = str(reply.score)
+                  rply = ('\n REPLY: '+ body + ' [author (' + author + ') score (' + score + ')]')
+                  tree = tree + rply
+        filewriter.writerow({'title': i.title,
+                             'author': i.author,
+                             'original content': i.is_original_content,
+                             'selfpost': i.is_self,
+                             'time created': ts,
+                             'stickied': i.stickied,
+                             'locked': i.locked,
+                             'NSFW': i.over_18,
+                             'selftext': i.selftext,
+                             'comment forest': tree,
+                             'number of comments': i.num_comments,
+                             'score': i.score,
+                             'upvote ratio': i.upvote_ratio,
+                             'permalink': i.permalink,
+                             'url': i.url})
 
 print ('We did it!')
